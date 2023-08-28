b4bfb33d47225e4919a2064576db857c533e6c65
Andrew <abarrett879@gmail.com>
2023-08-28 12:23:49 -0500

    Basic hero commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// gsap.set(".andrew-barrett", { autoAlpha: 0 });
+
+// function marketingAnimation() {
+//   let tl = gsap.timeline({
+//     defaults: { duration: 0.3, ease: Linear.easeNone },
+//   });
+//   tl.to(".andrew-barrett", { autoAlpha: 1 })
+//     .fromTo(
+//       ".andrew-barrett #mask-A",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+//     .fromTo(
+//       ".andrew-barrett #mask-n",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+//     .fromTo(
+//       ".andrew-barrett #mask-d",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+//     .fromTo(
+//       ".andrew-barrett #mask-r",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+//     .fromTo(
+//       ".andrew-barrett #mask-e",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+//     .fromTo(
+//       ".andrew-barrett #mask-w",
+//       {
+//         drawSVG: "0% 0%",
+//       },
+//       { drawSVG: true }
+//     )
+
+//     .to(".andrew-barrett", { autoAlpha: 0 }, "+=0.18");
+//   return tl;
+// }
+
+// window.onload = function () {
+//   marketingAnimation();
+// };
