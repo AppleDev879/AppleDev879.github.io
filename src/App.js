8232b069bdf78872f48d025addd771a63362b913
Andrew Barrett <andrew.barrett@peoplestrategy.com>
2023-09-01 20:55:38 -0500

    Revert "Idk"
    This reverts commit da0d2cf9581d50d5a444091e663e9bfde126f801, reversing
    changes made to d0c0d94fcedf0c9ca5dc0e30b19a97405c672396.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import SiteNav from "./components/nav/SiteNav";
+import Hero from "./components/hero/Hero";
+import Experience from "./components/experience/Experience";
+import Languages from "./components/languages/Languages";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <SiteNav />
+      </header>
+      <main>
+        <Hero />
+        <Experience />
+        <Languages />
+      </main>
+    </div>
+  );
+}
+
+export default App;
