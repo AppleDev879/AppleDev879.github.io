58f9f12b75a5512279f45f6e133c28be9971ce41
Andrew Barrett <andrew.barrett@peoplestrategy.com>
2023-09-27 10:04:22 -0500

    Add missing keys

--- src/components/languages/Item.jsx
+++ src/components/languages/Item.jsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 export default function Item({ language }) {
   let itemComps = [
-    <img className="lang-img" src={language.photo_url} alt="" />,
+    <img key="lang-img" className="lang-img" src={language.photo_url} alt="" />,
   ];
   if (language.display_name) {
-    itemComps.push(<p className="lang-title">{language.name}</p>);
+    itemComps.push(
+      <p key="lang-title" className="lang-title">
+        {language.name}
+      </p>
+    );
   }
   return <div className="grid-item">{itemComps}</div>;
 }
